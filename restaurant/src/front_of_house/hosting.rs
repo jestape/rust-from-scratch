f09dad0a51271b605ca2702b3acd935aa07da02a
Jordi Estapé <jestape@protonmail.com>
2019-07-15 10:26:38 +0200

    implement restaurant rustbook section 7

---
+++ restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,4 @@
+pub fn add_to_waitlist() { println!("Add to the waitlist")}
+fn seat_at_table() {}
+
+
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/Users/jordiestapecanal/Desktop/rust-from-scratch/restaurant/target/debug/librestaurant.rlib: /Users/jordiestapecanal/Desktop/rust-from-scratch/restaurant/src/front_of_house/hosting.rs /Users/jordiestapecanal/Desktop/rust-from-scratch/restaurant/src/front_of_house.rs /Users/jordiestapecanal/Desktop/rust-from-scratch/restaurant/src/lib.rs
